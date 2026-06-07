03eacfe78681d4843eea0d5827595a2764a7e906
Arman Rai <armanr@live.wsd1.orggit add .git config --global user.email armanr@live.wsd1.org>
2026-06-07 14:28:17 -0500

    updated player file

--- src/Player.java
+++ src/Player.java
@@ -1,60 +1,73 @@
 public class Player {
     private Position position;
     private Size size;
+    private int lives;
+    private Position spawnPosition;
 
     public Player(Position pos, Size size) {
         this.position = pos;
         this.size = size;
+        this.lives = 3;
+        this.spawnPosition = new Position(pos.X(), pos.Y());
     }
 
-    public void moveLeft(Boundary boundary) {
-        int newX;
-        if (position.X() < 1) {
-            newX = boundary.getGridWidth() - size.width();
+    public void moveLeft(Boundary boundary, MazeMap map) {
+        int newX = position.X() - 1;
+        if (newX >= 0 && !map.isWall(newX, position.Y())) {
+            position.setPosition(newX, position.Y());
         }
-        else {
-            newX = position.X() - 1;
-        }
-        position.setPosition(newX, position.Y());
     }
 
-    public void moveRight(Boundary boundary) {
-        int newX;
-        if (position.X() + size.width() > boundary.getGridWidth() - 1) {
-            newX = 0;
+    public void moveRight(Boundary boundary, MazeMap map) {
+        int newX = position.X() + 1;
+        if (newX + size.width() <= boundary.getGridWidth() && !map.isWall(newX, position.Y())) {
+            position.setPosition(newX, position.Y());
         }
-        else {
-            newX = position.X() + 1;
-        }
-        position.setPosition(newX, position.Y());
     }
 
-    public void moveUp(Boundary boundary) {
-        int newY;
-        if (position.Y() < 1) {
-            newY = boundary.getGridHeight() - size.height();
+    public void moveUp(Boundary boundary, MazeMap map) {
+        int newY = position.Y() - 1;
+        if (newY >= 0 && !map.isWall(position.X(), newY)) {
+            position.setPosition(position.X(), newY);
         }
-        else {
-            newY = position.Y() - 1;
-        }
-        position.setPosition(position.X(), newY);
     }
 
-    public void moveDown(Boundary boundary) {
-        int newY;
-        if (position.Y() + size.height() > boundary.getGridHeight() - 1) {
-            newY = 0;
+    public void moveDown(Boundary boundary, MazeMap map) {
+        int newY = position.Y() + 1;
+        if (newY + size.height() <= boundary.getGridHeight() && !map.isWall(position.X(), newY)) {
+            position.setPosition(position.X(), newY);
         }
-        else {
-            newY = position.Y() + 1;
-        }
-        position.setPosition(position.X(), newY);
+    }
+
+    public void loseLife() {
+        lives--;
+        respawn();
+    }
+
+    public void respawn() {
+        position.setPosition(spawnPosition.X(), spawnPosition.Y());
+    }
+
+    public boolean isAlive() {
+        return lives > 0;
+    }
+
+    public int getLives() {
+        return lives;
     }
 
     public Position getPosition() {
         return position;
     }
+
     public Size getSize() {
         return size;
+    }
+
+    public boolean collidesWith(Position otherPos, Size otherSize) {
+        return position.X() < otherPos.X() + otherSize.width()
+                && position.X() + size.width() > otherPos.X()
+                && position.Y() < otherPos.Y() + otherSize.height()
+                && position.Y() + size.height() > otherPos.Y();
     }
 }
